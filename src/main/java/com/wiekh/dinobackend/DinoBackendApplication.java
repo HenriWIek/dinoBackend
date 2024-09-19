800ed76b08d8f07d16d060555c78236d21926322
Henri <19638@nordakademie.de>
2024-09-19 17:40:02 +0200

    innitial commit

---
+++ src/main/java/com/wiekh/dinobackend/DinoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wiekh.dinobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiekh/dinobackend/DinoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiekh.dinobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
